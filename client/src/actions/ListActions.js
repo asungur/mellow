59fdb9ada1b87c2293159aa4069c5c0692fde449
Alican Sungur <sunguralican@gmail.com>
2022-02-21 16:24:29 +0000

    bugfix: render upon list creation

--- client/src/actions/ListActions.js
+++ client/src/actions/ListActions.js
@@ -2,13 +2,13 @@ import apiClient from "../lib/ApiClient";
 import * as types from "../constants/ActionTypes";
 
 function createListSuccess(list) {
-  return { type: 'CREATE_LIST_SUCCESS', list };
+  return { type: types.CREATE_LIST_SUCCESS, list };
 }
 
 export function createList(title, boardId) {
   return function(dispatch) {
-    apiClient.createList({ list: { title, boardId }}, list => {
-      dispatch(createListSuccess(list));
+    apiClient.createList({ list: { title, boardId }}, data => {
+      dispatch(createListSuccess(data.list));
     });
   };
 }
